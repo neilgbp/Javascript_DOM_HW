cd59314be61cf73861c6e5fae4696e5fd6a54e8d
NeilP <neilgbpate1@gmail.com>
2019-03-13 20:44:06 -0400

    Update app.js with data

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,17 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
+var submit = d3.select("#filterButton");
 
-// YOUR CODE HERE!
+
+// on startup - load data table into the html page
+function onStart() {
+    data.forEach((entry) => {
+        var row = tbody.append("tr");
+        Object.entries(entry).forEach(([key, value]) => {
+            var cell = tbody.append("td");
+            cell.text(value);
+        });
+    });
+}
+onStart();
